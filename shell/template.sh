53405b0af6d863878ebd971bfe850fe800a9105c
North Nekogreen <105654260+nekogreen@users.noreply.github.com>
2022-08-01 19:31:28 +0900

    Update shell.

--- shell/template.sh
+++ shell/template.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
-example() {
-  WORD=$1
-  echo "${WORD}"
-}
-
 usage() {
-  OPTIONS=$(grep -E -o '^while getopts [^ ]+' <"$0" | awk '{print $3}')
-  example "Usage: $0 [-${OPTIONS}]" 1>&2
+  echo "Usage: $0 [OPTION] ${1:-[PARAMETER]}"
   exit 1
 }
 
-while getopts hx: OPT; do
+while getopts :u: OPT; do
   case ${OPT} in
-  x)
-    EXAMPLE_OPT=yes
-    EXAMPLE_OPT_ARG="${OPTARG}"
-    ;;
-  h)
-    usage
+  u)
+    usage "${OPTARG}"
     ;;
   \?)
     usage
     ;;
   esac
 done
-
 shift $((OPTIND - 1))
-
-[ "x${EXAMPLE_OPT:-no}" == "xyes" ] && example "${EXAMPLE_OPT_ARG}"
+usage
