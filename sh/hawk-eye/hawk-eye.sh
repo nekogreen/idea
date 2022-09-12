a0f69f7610d5c55a2834219879b587b32664f100
North Nekogreen <105654260+nekogreen@users.noreply.github.com>
2022-09-13 00:29:50 +0900

    Remove word x.

--- sh/hawk-eye/hawk-eye.sh
+++ sh/hawk-eye/hawk-eye.sh
@@ -6,19 +6,20 @@ hawk_eye() {
     cd "${PROJECT_ROOT_PATH}"
     PROJECT_ROOT=$(basename "${PROJECT_ROOT_PATH}")
     [ -e .git ] || continue
+
     git fetch
     CURRENT_BRANCH=$(git branch --show-current)
     HAS_REMOTE_DEVELOP=$(git branch -a | grep -e 'remotes/upstream/develop' >/dev/null 2>&1 && echo true || echo false)
     GIT_DIFF=$(git diff --exit-code --quiet && echo "-" || echo "+")
-    if [ "x${GIT_DIFF}" == "x+" ]; then
+    if [ "${GIT_DIFF}" == "+" ]; then
       echo "${PROJECT_ROOT} ${CURRENT_BRANCH} ${GIT_DIFF}"
       continue
     fi
-    if [ "x${CURRENT_BRANCH}" == "xmaster" ] && ${HAS_REMOTE_DEVELOP}; then
+    if [ "${CURRENT_BRANCH}" == "master" ] && ${HAS_REMOTE_DEVELOP}; then
       echo "${PROJECT_ROOT} ${CURRENT_BRANCH} ${GIT_DIFF}"
       continue
     fi
-    if [ "x${CURRENT_BRANCH}" != "xmaster" ] && [ "x${CURRENT_BRANCH}" != "xdevelop" ]; then
+    if [ "${CURRENT_BRANCH}" != "master" ] && [ "${CURRENT_BRANCH}" != "develop" ]; then
       echo "${PROJECT_ROOT} ${CURRENT_BRANCH} ${GIT_DIFF}"
       continue
     fi
